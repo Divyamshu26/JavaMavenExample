e013a81886cbb19f56b10ded5ae6551fe93dfefa
giyyardivyamshuraghavendra <divyamshu.raghavendra@gmail.com>
2024-11-14 12:19:38 +0530

    file added

---
+++ src/main/java/com/mavenapp/demo/JavaMavenExample/App.java
@@ -0,0 +1,10 @@
+package com.mavenapp.demo.JavaMavenExample;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Divyamshu 22BD1A054M");
+    }
+}
